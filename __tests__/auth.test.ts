3d158a213962590d6201589cd3381fcecfc5cddb
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-25 18:56:16 -0500

    Put some mock tests

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -2,4 +2,8 @@ describe("checking auth suite to make sure it functions properly", () => {
   test("the addition of 1 and 1", () => {
     expect(1 + 1).toBe(2);
   });
+
+  test("the subtraction of 1 and 1", () => {
+    expect(1 - 1).toBe(0);
+  });
 });
